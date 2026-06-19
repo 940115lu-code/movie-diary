683ce0b742a8942b8e4c335b9875444d751318dd
Daniel LU <daniellu@134-208-38-35.ndhu.edu.tw>
2026-06-19 23:59:00 +0800

    Update score style

--- movies.js
+++ movies.js
@@ -4,90 +4,90 @@ const movies = [
     poster: "posters/sorry-baby.jpg",
     date: "2026-06-05",
     rating: 9.5,
-    review: "master piece"
+    review: "Master piece 每個人都應該看的療傷作品"
   },
   {
     title: "Project Hail Mary",
     poster: "posters/project-hail-mary.jpg",
     date: "2026-03-18",
     rating: 9.2,
-    review: "頂中頂之後要去看小說"
+    review: "電影＆小說都給我去看"
   },
   {
     title: "The Drama",
     poster: "posters/the-drama.jpg",
     date: "2026-04-02",
     rating: 8.8,
-    review: "愛情裡該完全坦誠？！"
+    review: "讓人思考很久的人生課題, 音樂和拍攝手法都很符合我的胃口"
   },
   {
     title: "Backroom",
     poster: "posters/backroom.jpg",
     date: "2026-05-28",
     rating: 8.5,
-    review: "畫面帶出心裡恐懼"
+    review: "就像做了一場夢, 感覺自己也深陷其中"
   },
   {
     title: "Colony",
     poster: "posters/colony.jpg",
     date: "2026-05-23",
     rating: 8,
-    review: "殭屍設定很創新 1分具教煥加上去的"
+    review: "殭屍片的新創意"
   },
   {
     title: "The Devil Wears Prada 2",
     poster: "posters/the-devil-wears-prada-2.jpg",
     date: "2026-04-29",
     rating: 7.8,
-    review: "經典 聽覺視覺都很滿足"
+    review: "經典的畫面重現, 眼睛耳朵都會有驚喜"
   },
   {
     title: "Michael",
     poster: "posters/michael.jpg",
     date: "2026-04-22",
     rating: 7.5,
-    review: "全靠MJ拿下的分數"
+    review: "就像聽了一場MJ的演唱會"
   },
   {
     title: "Wuthering Heights",
     poster: "posters/wuthering-heights.jpg",
     date: "2026-02-28",
     rating: 7.3,
-    review: "很虐"
+    review: "很喜歡這種文學經典的改編, 提醒自己有空記得看小說"
   },
   {
     title: "Mother Mary",
     poster: "posters/mother-mary.jpg",
     date: "2026-04-17",
     rating: 7.3,
-    review: "做自己"
+    review: "友情,親情還有自我的意象思考, 畫面很唯美 "
   },
   {
     title: "陽光女子合唱團",
     poster: "posters/sunny-girls.jpg",
     date: "2026-02-06",
     rating: 7,
-    review: "如果結局停在表演牽手那邊可以超過8"
+    review: "抱著眼淚看完的一部片, 很想哭"
   },
   {
     title: "Lee Cronin's The Mummy",
     poster: "posters/lee-cronins-the-mummy.jpg",
     date: "2026-04-23",
     rating: 6.8,
-    review: ""
+    review: "其實木乃伊的設定真的很不錯"
   },
   {
     title: "Top Gun",
     poster: "posters/top-gun.jpg",
     date: "2026-05-16",
     rating: 6.8,
-    review: "重映"
+    review: "在1986能拍出這種畫面真的很厲害"
   },
   {
     title: "Couture",
     poster: "posters/couture.jpg",
     date: "2026-05-09",
-    rating: 4,
-    review: ""
+    rating: 4.5,
+    review: "要感人劇情有畫面, 要愛情故事有畫面"
   }
 ];
